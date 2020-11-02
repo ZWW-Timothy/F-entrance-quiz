2d6bb2a4703136a28e1d09b792a8e7a95b3fc4d7
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-11-02 14:18:29 +0800

    [mengqiu] add more comments

--- src/App/GroupList.js
+++ src/App/GroupList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './GroupList.scss';
 // TODO GTB-工程实践: - 每个组件应该使用一个独立的文件夹把跟这个组件相关的文件包起来
 class GroupList extends Component {
+    // TODO GTB-工程实践: - API请求应该被抽取到一个单独的文件
     getGroupList = () => {
         fetch('http://localhost:8080/groupList', {
             method: "GET",
